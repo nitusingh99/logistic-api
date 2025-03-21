f6e4a8c6e7d85242089efc1fe0e2ca82b5f77bf0
nitusingh99 <138586315+nitusingh99@users.noreply.github.com>
2025-03-21 13:23:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 def check_inventory():
     return {"message": "Medical inventory data here!"}
 
+# Required to run Uvicorn when the script is executed directly
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
